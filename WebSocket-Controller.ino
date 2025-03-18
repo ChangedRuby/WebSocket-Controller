ef03721d96004572e36cad0dbf712831c46fc6cf
ChangedRuby <gugasboy7@gmail.com>
2025-03-18 17:42:42 -0300

    Changed file name
    Changed file name

--- WebSocket-Controller.ino
+++ WebSocket-Controller.ino
@@ -1,19 +1,22 @@
 #include <WiFi.h>
 #include <WebSocketsServer.h>
 
-#define btn_1 18
-#define btn_2 21
-#define btn_3 19
-#define btn_4 5
+// This controller uses 4 buttons to movement (Up, Down, Left, Right), feel free to change to any amount you would use or even implement a Joystick! :)
+#define btn_1 0 // FIRST DIGITAL PIN HERE
+#define btn_2 0 // FIRST DIGITAL PIN HERE
+#define btn_3 0 // FIRST DIGITAL PIN HERE
+#define btn_4 0 // FIRST DIGITAL PIN HERE
 
+// Flags to prevent repetitive sending of information.
 int flag_btn1 = 0;
 int flag_btn2 = 0;
 int flag_btn3 = 0;
 int flag_btn4 = 0;
 
 // Constants
-const char* ssid = "Goppas";
-const char* password = "senha123";
+// Name and Password are for the WiFi connection.
+const char* ssid = ""; // CONNECTION NAME HERE
+const char* password = ""; // PASSWORD HERE
 
 // Globals
 WebSocketsServer webSocket = WebSocketsServer(80);
@@ -44,7 +47,7 @@ void onWebSocketEvent(uint8_t num,
     // Echo text message back to client
     case WStype_TEXT:
       Serial.printf("[%u] Text: %s\n", num, payload);
-      webSocket.sendTXT(num, "hello godot");
+      webSocket.sendTXT(num, "Hello godot");
       Serial.println(num);
       break;
 
@@ -62,6 +65,7 @@ void onWebSocketEvent(uint8_t num,
 
 void setup() {
 
+// Setups pins in which the buttons are connected.
   pinMode(btn_1, INPUT_PULLUP);
   pinMode(btn_2, INPUT_PULLUP);
   pinMode(btn_3, INPUT_PULLUP);
@@ -73,6 +77,8 @@ void setup() {
   // Connect to access point
   Serial.println("Connecting");
   WiFi.begin(ssid, password);
+
+  // Prints '.' untils a connection is successful.
   while ( WiFi.status() != WL_CONNECTED ) {
     delay(500);
     Serial.print(".");
@@ -90,6 +96,7 @@ void setup() {
 
 void loop() {
 
+// Logic for each button, which then sends a message to the WebSocket if any change is made.
   if(flag_btn1 == 1 && debounce(flag_btn1, btn_1) == LOW){
         webSocket.sendTXT(0, "soltou 1\n");
         Serial.println("enviando LOW");
@@ -143,6 +150,7 @@ void loop() {
 
 }
 
+// Debounce is important to prevent small inconsistencies on the hardware of the the system.
 int debounce(int estado, int pino){
   int estadoAtual = digitalRead(pino);
   if(estado != estadoAtual){
